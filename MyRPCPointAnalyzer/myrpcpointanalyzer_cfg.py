147b06ad79dcf81987f520b0b7ca70b8230ed0b3
Piet Verwilligen <piet@blacksad.(none)>
2014-07-31 10:26:04 +0200

    first entry RPCPointAnalyzer

---
+++ MyRPCPointAnalyzer/myrpcpointanalyzer_cfg.py
@@ -0,0 +1,86 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("Demo")
+
+process.load("FWCore.MessageService.MessageLogger_cfi")
+# process.load("Geometry.MuonCommonData.muonIdealGeometryXML_cfi")
+# process.load("Geometry.CMSCommonData.cmsExtendedGeometryPostLS1XML_cfi")
+process.load("Configuration.Geometry.GeometryExtended2015_cff")
+process.load("Geometry.RPCGeometry.rpcGeometry_cfi")
+process.load("Geometry.CSCGeometry.cscGeometry_cfi")
+process.load("Geometry.DTGeometry.dtGeometry_cfi")
+process.load("Geometry.MuonNumbering.muonNumberingInitialization_cfi")
+
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+process.GlobalTag.globaltag = 'GR_R_71_V6::All'
+
+# process.GlobalTag.globaltag = 'MC_31X_V1::All'
+# process.GlobalTag.globaltag = 'GR_P_V32::All'
+
+
+
+
+
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(10) )
+
+process.source = cms.Source("PoolSource",
+    # replace 'myfile.root' with the source file you want to use
+    fileNames = cms.untracked.vstring(
+        ###  MC  ####
+        # 'file:/afs/cern.ch/user/p/piet/work/Analysis/CMSSW_6_0_1_PostLS1v1/src/cmsDriverCommands/SingleMuPt100_cfi_RECO.root'
+        ### MWGR1 ###
+        # MWGR1 :: old reco
+        'file:/afs/cern.ch/user/p/piet/work/Analysis/CMSSW_7_1_1/src/MyFilters/MyME4SegmentFilter/MWGR1_run222608_Skimmed.root',
+        # MWGR1 :: new reco
+        # 'file:/afs/cern.ch/user/p/piet/work/Analysis/CMSSW_7_1_1/src/MyFilters/MyME4SegmentFilter/MWGR1_run222608_NewFullReco_Skimmed.root',
+        # 'file:/afs/cern.ch/user/p/piet/work/Analysis/CMSSW_7_1_1/src/MyCmsDriverCommands/run222608_newfullreco/run222608_RECO_ls0001.root'    
+    )
+)
+
+process.rpcPointProducer = cms.EDProducer('RPCPointProducer',
+
+    incldt = cms.untracked.bool(True),
+    inclcsc = cms.untracked.bool(True),
+    incltrack =  cms.untracked.bool(False),
+                                          
+    debug = cms.untracked.bool(False),
+                                          
+    rangestrips = cms.untracked.double(4.),
+    rangestripsRB4 = cms.untracked.double(4.),
+    MinCosAng = cms.untracked.double(0.85),
+    MaxD = cms.untracked.double(80.0),
+    MaxDrb4 = cms.untracked.double(150.0),
+    ExtrapolatedRegion = cms.untracked.double(0.6), #in stripl/2 in Y and stripw*nstrips/2 in X
+    # cscSegments = cms.InputTag('dTandCSCSegmentsinTracks','SelectedCscSegments','OwnParticles'),
+    # dt4DSegments = cms.InputTag('dTandCSCSegmentsinTracks','SelectedDtSegments','OwnParticles'),
+    cscSegments = cms.InputTag("cscSegments"),
+    dt4DSegments = cms.InputTag("dt4DSegments"),                                          
+    tracks = cms.InputTag("standAloneMuons"),
+    TrackTransformer = cms.PSet(
+          DoPredictionsOnly = cms.bool(False),
+          Fitter = cms.string('KFFitterForRefitInsideOut'),
+          TrackerRecHitBuilder = cms.string('WithTrackAngle'),
+          Smoother = cms.string('KFSmootherForRefitInsideOut'),
+          MuonRecHitBuilder = cms.string('MuonRecHitBuilder'),
+          RefitDirection = cms.string('alongMomentum'),
+          RefitRPCHits = cms.bool(False),
+          Propagator = cms.string('SmartPropagatorAnyRKOpposite')
+    )
+)
+
+
+process.demo = cms.EDAnalyzer('MyRPCPointAnalyzer',
+
+      debug        = cms.untracked.bool(True),
+      RootFileName = cms.untracked.string("MyRPCPointHistograms_run222608_ls0001To0013_FirstReco.root"),
+      cscSegments  = cms.untracked.InputTag('cscSegments'),
+      dt4DSegments = cms.untracked.InputTag('dt4DSegments'),
+      rpcRecHits   = cms.untracked.InputTag("rpcRecHits"),
+      rpcDTPoints  = cms.untracked.InputTag("rpcPointProducer","RPCDTExtrapolatedPoints"),
+      rpcCSCPoints = cms.untracked.InputTag("rpcPointProducer","RPCCSCExtrapolatedPoints"),
+                              
+)
+
+# process.p = cms.Path(process.rpcPointProducer)
+process.p = cms.Path(process.rpcPointProducer*process.demo)
+
---
+++ MyRPCPointAnalyzer/plugins/MyRPCPointAnalyzer.cc
@@ -0,0 +1,404 @@
+// -*- C++ -*-
+//
+// Package:    MyRPCPointAnalyzer
+// Class:      MyRPCPointAnalyzer
+// 
+/**\class MyRPCPointAnalyzer MyRPCPointAnalyzer.cc MyAnalyzers/MyRPCPointAnalyzer/plugins/MyRPCPointAnalyzer.cc
+
+ Description: [one line class summary]
+
+ Implementation:
+     [Notes on implementation]
+*/
+//
+// Original Author:  Piet Verwilligen,161 R-006,+41227676292,
+//         Created:  Wed Oct 24 17:28:30 CEST 2012
+// $Id$
+//
+//
+
+
+// system include files
+#include <memory>
+#include <fstream>
+#include <sys/time.h>
+#include <string>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+
+
+// root include files
+#include "TFile.h"
+#include "TH1F.h"
+#include "TH2F.h"
+#include "TF1.h"
+#include "THStack.h"
+#include "TLegend.h"
+#include "TTree.h"
+#include "TCanvas.h"
+#include "TDirectoryFile.h"
+#include "TGraph.h"
+#include "TGraphErrors.h"
+#include "TGraphAsymmErrors.h"
+
+
+// user include files
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/EDAnalyzer.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/ESHandle.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/ServiceRegistry/interface/Service.h"
+
+#include <DataFormats/RPCRecHit/interface/RPCRecHit.h>
+#include "DataFormats/RPCRecHit/interface/RPCRecHitCollection.h"
+#include <DataFormats/RPCDigi/interface/RPCDigiCollection.h>
+#include "SimDataFormats/TrackingHit/interface/PSimHitContainer.h"
+ 
+#include "Geometry/RPCGeometry/interface/RPCGeometry.h"
+#include <Geometry/RPCGeometry/interface/RPCRoll.h>
+#include <Geometry/Records/interface/MuonGeometryRecord.h>
+#include <Geometry/RPCGeometry/interface/RPCGeomServ.h>
+#include <Geometry/CommonDetUnit/interface/GeomDet.h>
+#include "DataFormats/Provenance/interface/Timestamp.h"
+
+
+#include "DQMServices/Core/interface/DQMStore.h"
+#include "DQMServices/Core/interface/MonitorElement.h"
+
+#include <Geometry/RPCGeometry/interface/RPCGeometry.h>
+#include <Geometry/DTGeometry/interface/DTGeometry.h>
+#include <Geometry/CSCGeometry/interface/CSCGeometry.h>
+
+
+#include <DataFormats/RPCDigi/interface/RPCDigiCollection.h>
+#include "DataFormats/RPCRecHit/interface/RPCRecHitCollection.h"
+#include <DataFormats/MuonDetId/interface/RPCDetId.h>
+#include <DataFormats/DTRecHit/interface/DTRecSegment4DCollection.h>
+#include <DataFormats/CSCRecHit/interface/CSCSegmentCollection.h>
+#include <Geometry/RPCGeometry/interface/RPCGeomServ.h>
+#include <Geometry/CommonDetUnit/interface/GeomDet.h>
+#include <Geometry/Records/interface/MuonGeometryRecord.h>
+#include <Geometry/CommonTopologies/interface/RectangularStripTopology.h>
+#include <Geometry/CommonTopologies/interface/TrapezoidalStripTopology.h>
+
+
+
+//
+// class declaration
+//
+
+class MyRPCPointAnalyzer : public edm::EDAnalyzer {
+   public:
+      explicit MyRPCPointAnalyzer(const edm::ParameterSet&);
+      ~MyRPCPointAnalyzer();
+
+      static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
+
+
+   private:
+      virtual void beginJob() ;
+      virtual void analyze(const edm::Event&, const edm::EventSetup&);
+      virtual void endJob() ;
+
+      virtual void beginRun(edm::Run const&, edm::EventSetup const&);
+      //virtual void endRun(edm::Run const&, edm::EventSetup const&);
+      //virtual void beginLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&);
+      //virtual void endLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&);
+
+      // ----------member data ---------------------------
+  edm::ESHandle <RPCGeometry> rpcGeom;
+  edm::ESHandle <DTGeometry>  dtGeom;
+  edm::ESHandle <CSCGeometry> cscGeom;
+
+  bool debug;
+  std::string rootFileName;
+  TFile * outputfile;
+
+  edm::InputTag cscSegments;
+  edm::InputTag dt4DSegments;
+  edm::InputTag rpcRecHitsLabel;
+  edm::InputTag rpcDTPointsLabel;
+  edm::InputTag rpcCSCPointsLabel;
+
+};
+
+//
+// constants, enums and typedefs
+//
+
+
+//
+// static data member definitions
+//
+
+//
+// constructors and destructor
+//
+MyRPCPointAnalyzer::MyRPCPointAnalyzer(const edm::ParameterSet& iConfig)
+
+{
+   //now do what ever initialization is needed
+
+  debug             = iConfig.getUntrackedParameter<bool>("debug",false);
+
+  cscSegments       = iConfig.getUntrackedParameter<edm::InputTag>("cscSegments");
+  dt4DSegments      = iConfig.getUntrackedParameter<edm::InputTag>("dt4DSegments");
+  rpcRecHitsLabel   = iConfig.getUntrackedParameter<edm::InputTag>("rpcRecHits");
+  rpcDTPointsLabel  = iConfig.getUntrackedParameter<edm::InputTag>("rpcDTPoints");
+  rpcCSCPointsLabel = iConfig.getUntrackedParameter<edm::InputTag>("rpcCSCPoints");
+
+  rootFileName      = iConfig.getUntrackedParameter<std::string>("RootFileName");
+
+  outputfile = new TFile(rootFileName.c_str(), "RECREATE" );
+
+}
+
+
+MyRPCPointAnalyzer::~MyRPCPointAnalyzer()
+{
+ 
+   // do anything here that needs to be done at desctruction time
+   // (e.g. close files, deallocate resources etc.)
+  outputfile->cd();
+
+}
+
+
+//
+// member functions
+//
+
+// ------------ method called for each event  ------------
+void
+MyRPCPointAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
+{
+
+  int evtNum = (iEvent.id()).event();
+  int lumNum = (iEvent.id()).luminosityBlock();
+  int runNum = (iEvent.id()).run();
+
+  if(debug) std::cout<<"----- Run "<<std::setw(7)<<runNum<<" Lumi "<<std::setw(3)<<lumNum<<" Event "<<std::setw(12)<<evtNum<<" ---------------------------------------------------------------------------"<<std::endl;
+
+  edm::Handle<DTRecSegment4DCollection> allDTSegments;
+  iEvent.getByLabel(dt4DSegments, allDTSegments);
+
+  edm::Handle<CSCSegmentCollection> allCSCSegments;
+  iEvent.getByLabel(cscSegments, allCSCSegments);
+
+  edm::Handle<RPCRecHitCollection> rpcRecHits;
+  iEvent.getByLabel(rpcRecHitsLabel,rpcRecHits);
+
+  edm::Handle<RPCRecHitCollection> rpcDTPoints;
+  iEvent.getByLabel(rpcDTPointsLabel,rpcDTPoints);
+
+  edm::Handle<RPCRecHitCollection> rpcCSCPoints;
+  iEvent.getByLabel(rpcCSCPointsLabel,rpcCSCPoints);
+
+  // ================
+  // Collection Sizes
+  // ================
+  if(debug) {
+    std::cout<<"Amount of DT | CSC | RPC Segments/Hits and DT | CSC Extrap in this Event: [ ";
+    std::cout<<allDTSegments->size()<<" | "<<allCSCSegments->size()<<" | "<<rpcRecHits->size()<<" | "<<rpcDTPoints->size()<<" | "<<rpcCSCPoints->size()<<" ]"<<std::endl;
+  }
+
+
+  // =========================================
+  // Quick Overview Segments in ME4
+  // =========================================
+  if(debug) {
+    CSCSegmentCollection::const_iterator segment;
+    for (segment = allCSCSegments->begin();segment!=allCSCSegments->end(); ++segment){
+      // CSCDetId CSCId = segment->cscDetId();
+      if((segment->cscDetId()).station()==4) {
+	std::cout<<"CSC Segment in "<<segment->cscDetId()<<std::endl;
+      }
+    }
+  }
+
+
+  // =========================================
+  // Quick Overview Extrapolated Points in RE4
+  // =========================================
+  if(debug) {
+    RPCRecHitCollection::const_iterator point;
+    for(point = rpcDTPoints->begin();  point != rpcDTPoints->end();  point++) { 
+      // RPCGeomServ srv(point->rpcId()); std::cout<<"DT Extapolated Point in "  <<(point->rpcId()).rawId()<<" = "<<srv.name()<<std::endl;
+    }
+    for(point = rpcCSCPoints->begin(); point != rpcCSCPoints->end(); point++) { 
+      if((point->rpcId()).station()==4) {
+	RPCGeomServ srv(point->rpcId()); std::cout<<"CSC Extapolated Point in " <<(point->rpcId()).rawId()<<" = "<<srv.name()<<std::endl;}
+    }
+    std::cout<<"RPC Rechits in { ";
+    for(point = rpcRecHits->begin();   point != rpcRecHits->end();   point++) { 
+      if((point->rpcId()).station()==4 && (point->rpcId()).region()!=0){
+	std::cout<<(point->rpcId()).rawId()<<" ";   
+      }
+    }
+    std::cout<<" }"<<std::endl;
+    std::cout<<"RPC Rechits in { ";
+    for(point = rpcRecHits->begin();   point != rpcRecHits->end();   point++) { 
+      if((point->rpcId()).station()==4 && (point->rpcId()).region()!=0){
+	RPCGeomServ srv(point->rpcId()); std::cout<<srv.name()<<" "; 
+      }
+    }
+    std::cout<<" }"<<std::endl;
+  }
+  // ================
+  // RPC recHits
+  // ================
+  // edm::Handle<RPCRecHitCollection> rpcRecHits; 
+  // iEvent.getByLabel("rpcRecHits","",rpcRecHits);
+  RPCRecHitCollection::const_iterator recHit;
+  for (recHit = rpcRecHits->begin(); recHit != rpcRecHits->end(); recHit++) {
+    RPCDetId rollId = (RPCDetId)(*recHit).rpcId();
+    RPCGeomServ rollsrv(rollId);
+    std::string nameRoll = rollsrv.name();
+
+    LocalPoint RPCLocalPoint=recHit->localPosition();
+    // LocalError recHitErr=recHit->localPositionError();
+    const RPCRoll* Roll = rpcGeom->roll(rollId);
+    const BoundPlane & RPCSurface = Roll->surface(); 
+    GlobalPoint RPCGlobalPoint = RPCSurface.toGlobal(RPCLocalPoint);
+
+    // ==========================
+    // DT extrapolated points
+    // ========================== 
+    if(rpcDTPoints.isValid() && rpcDTPoints->begin()!=rpcDTPoints->end()) {
+
+      RPCRecHitCollection::const_iterator rpcPoint;
+      for(rpcPoint = rpcDTPoints->begin(); rpcPoint != rpcDTPoints->end(); rpcPoint++){
+
+	// Only details if there is an extrapolated point in this DetId
+	RPCDetId  rpcId = rpcPoint->rpcId();
+	if(rpcId.rawId() != rollId.rawId()) continue;
+
+	// RPC DetId and Extrapolated DetId match
+	RPCGeomServ rpcsrv(rpcId);
+	std::string nameRPC = rpcsrv.name();
+
+	const RPCRoll* DTExtrap_Roll           = rpcGeom->roll(rpcId);
+	LocalPoint DTExtrap_RPCLocalPoint      = rpcPoint->localPosition();
+	const BoundPlane & DTExtrap_RPCSurface = DTExtrap_Roll->surface();
+	GlobalPoint DTExtrap_RPCGlobalPoint    = DTExtrap_RPCSurface.toGlobal(DTExtrap_RPCLocalPoint);
+	// const float stripPredicted =DTExtrap_Roll->strip(LocalPoint(DTExtrap_RPCLocalPoint.x(),DTExtrap_RPCLocalPoint.y(),0.));
+	const float stripPredicted =DTExtrap_Roll->strip(DTExtrap_RPCLocalPoint);
+
+	if(debug) {
+	  // Information about original RPC Rechit
+	  std::cout<<"RPC Rec Hit in "    <<rollId.rawId()<<" = "<<nameRoll;
+	  std::cout<<" Local Position = " <<RPCLocalPoint;//<<" Local Uncrt = "<<
+	  std::cout<<" Global Position = "<<RPCGlobalPoint; // <<" Global Uncrt = "<<;
+	  std::cout<<" Clustersize = "    <<recHit->clusterSize()<<" First strip of cluster = "<<recHit->firstClusterStrip()<<" BX = "<<recHit->BunchX()<<std::endl;
+	  // Information about extrapolated RPC Point
+	  std::cout<<"RPC Ext Pnt in "    <<rpcId.rawId()<<" = "<<nameRPC;
+	  std::cout<<" Local Position  = "<<DTExtrap_RPCLocalPoint;
+	  std::cout<<" Global Position = "<<DTExtrap_RPCGlobalPoint;
+	  std::cout<<" Strip Predicted = "<<stripPredicted<<std::endl;
+	}
+      } // End Loop over DT Extrap Points
+    } // End Check whether DT Extrap Points are empty
+
+    // ==========================
+    // CSC extrapolated points
+    // ==========================
+    if(rpcCSCPoints.isValid()) if(rpcCSCPoints->begin()!=rpcCSCPoints->end()) {
+
+      RPCRecHitCollection::const_iterator rpcPoint;
+      for(rpcPoint = rpcCSCPoints->begin(); rpcPoint != rpcCSCPoints->end(); rpcPoint++){
+
+	// Only details if there is an extrapolated point in this DetId
+	RPCDetId  rpcId = rpcPoint->rpcId();
+	if(rpcId.rawId() != rollId.rawId()) continue;
+
+	// RPC DetId and Extrapolated DetId match
+	RPCGeomServ rpcsrv(rpcId);
+	std::string nameRPC = rpcsrv.name();
+
+	const RPCRoll* CSCExtrap_Roll           = rpcGeom->roll(rpcId);
+	LocalPoint CSCExtrap_RPCLocalPoint      = rpcPoint->localPosition();
+	const BoundPlane & CSCExtrap_RPCSurface = CSCExtrap_Roll->surface();
+	GlobalPoint CSCExtrap_RPCGlobalPoint    = CSCExtrap_RPCSurface.toGlobal(CSCExtrap_RPCLocalPoint);
+	// const float stripPredicted =CSCExtrap_Roll->strip(LocalPoint(CSCExtrap_RPCLocalPoint.x(),CSCExtrap_RPCLocalPoint.y(),0.));
+	const float stripPredicted =CSCExtrap_Roll->strip(CSCExtrap_RPCLocalPoint);
+
+	if(debug) {
+	  // Information about original RPC Rechit
+	  std::cout<<"RPC Rec Hit in "    <<rollId.rawId()<<" = "<<nameRoll;
+	  std::cout<<" Local Position = " <<RPCLocalPoint;//<<" Local Uncrt = "<<
+	  std::cout<<" Global Position = "<<RPCGlobalPoint; // <<" Global Uncrt = "<<;
+	  std::cout<<" Clustersize = "    <<recHit->clusterSize()<<" First strip of cluster = "<<recHit->firstClusterStrip()<<" BX = "<<recHit->BunchX()<<std::endl;
+	  // Information about extrapolated RPC Point
+	  std::cout<<"RPC Ext Pnt in "    <<rpcId.rawId()<<" = "<<nameRPC;
+	  std::cout<<" Local Position  = "<<CSCExtrap_RPCLocalPoint;
+	  std::cout<<" Global Position = "<<CSCExtrap_RPCGlobalPoint;
+	  std::cout<<" Strip Predicted = "<<stripPredicted<<std::endl;
+	}
+
+      } // End Loop over CSC Extrap Points
+    } // End Check whether CSC Extrap Points are empty
+
+  } // End Loop over RPC Rechits
+
+  if(debug) std::cout<<"------------------------------------------------------------------------------------------------------------------------"<<std::endl;
+}
+
+
+// ------------ method called once each job just before starting event loop  ------------
+void 
+MyRPCPointAnalyzer::beginJob()
+{
+}
+
+// ------------ method called once each job just after ending the event loop  ------------
+void 
+MyRPCPointAnalyzer::endJob() 
+{
+}
+
+// ------------ method called when starting to processes a run  ------------
+void 
+MyRPCPointAnalyzer::beginRun(edm::Run const& iRun, edm::EventSetup const& iSetup)
+{
+  iSetup.get<MuonGeometryRecord>().get(rpcGeom);
+}
+
+// ------------ method called when ending the processing of a run  ------------
+/*
+void 
+MyRPCPointAnalyzer::endRun(edm::Run const&, edm::EventSetup const&)
+{
+}
+*/
+
+// ------------ method called when starting to processes a luminosity block  ------------
+/*
+void 
+MyRPCPointAnalyzer::beginLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&)
+{
+}
+*/
+
+// ------------ method called when ending the processing of a luminosity block  ------------
+/*
+void 
+MyRPCPointAnalyzer::endLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&)
+{
+}
+*/
+
+// ------------ method fills 'descriptions' with the allowed parameters for the module  ------------
+void
+MyRPCPointAnalyzer::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
+  //The following says we do not know what parameters are allowed so do no validation
+  // Please change this to state exactly what you do use, even if it is no parameters
+  edm::ParameterSetDescription desc;
+  desc.setUnknown();
+  descriptions.addDefault(desc);
+}
+
+//define this as a plug-in
+DEFINE_FWK_MODULE(MyRPCPointAnalyzer);
