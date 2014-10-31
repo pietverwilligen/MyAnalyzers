f527138058263fc86c7603a81dd778121eec1e48
Piet Verwilligen <piet@blacksad.(none)>
2014-10-31 19:25:45 +0100

    72X GT

--- RPCTiming/rpctiming_cfg.py
+++ RPCTiming/rpctiming_cfg.py
@@ -3,8 +3,9 @@ import FWCore.ParameterSet.Config as cms
 process = cms.Process("Timing")
 
 # process.load("Geometry.CMSCommonData.cmsExtendedGeometry2023XML_cfi")
-process.load("Configuration.Geometry.GeometryExtended2023_cff")
-process.load("Configuration.Geometry.GeometryExtended2023Reco_cff")
+# process.load("Configuration.Geometry.GeometryExtended2023_cff")
+# process.load("Configuration.Geometry.GeometryExtended2023Reco_cff")
+process.load("Configuration.Geometry.GeometryExtended2015Reco_RPC4RE11_cff")
 # process.load("Configuration.Geometry.GeometryExtended2023RPCEtaUpscope_cff")
 # process.load('Configuration.Geometry.GeometryExtended2023RPCEtaUpscopeReco_cff')
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
@@ -12,7 +13,9 @@ process.load("FWCore.MessageService.MessageLogger_cfi")
 process.load("Geometry.RPCGeometry.rpcGeometry_cfi")
 
 from Configuration.AlCa.GlobalTag import GlobalTag
-process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:upgrade2019', '')
+# process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:upgrade2019', '')
+# process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:upgrade2023', '')
+process.GlobalTag = GlobalTag(process.GlobalTag, 'MC_72_V1', '')
 
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1) )
 
